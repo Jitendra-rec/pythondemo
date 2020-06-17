465309a9fb9dcc169103676607421607dcc24705
Jitendra-rec <kjitendra4500@gmail.com>
2020-06-17 07:12:07 -1200

    this is what duck typing in polymorph

--- Day 14/ducktyping.py
+++ Day 14/ducktyping.py
@@ -1,1 +1,18 @@
-print('ducktyping')
+class student:
+    def study(self):
+        print('IN student class')
+        print('depends on teachers')
+class programer:
+    def study(self):
+        print('In programer class')
+        print('for developing new skilss')
+
+class teacher:
+    def getup(self,g):
+        g.study()
+
+l1=student()
+l2=programer()
+teah=teacher()
+teah.getup(l1)
+teah.getup(l2)
