c2212bafa916f699c1fd04899bb6553ef86d57c4
Jitendra-rec <kjitendra4500@gmail.com>
2020-06-18 23:13:43 -1200

    learning tkinter pakage

---
+++ tkinter project/tk1.py
@@ -0,0 +1,3 @@
+from tkinter import*
+rahul = Tk()
+rahul.mainloop()
---
+++ tkinter project/tk2.py
@@ -0,0 +1,8 @@
+from tkinter import *
+a=Tk()
+a.geometry("733x434")
+h=Label(text="WELCOME TO PYCHARM")
+a.minsize(733,434)
+a.maxsize(733,434)
+h.pack()
+a.mainloop()
